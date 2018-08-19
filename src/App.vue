ca2cbf183bc4891b0e731636914410d86cb41436
Yurisa <779509411@qq.com>
2018-08-19 18:45:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { get } from '@/util'
+export default {
+  async created () {
+    console.log('小程序启动了')
+    const res = await get('/weapp/test')
+    console.log(res)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
